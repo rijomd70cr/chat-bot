180b02d429457e396b0d3edbf3b4a7506f517b2f
rijomd70cr <rijo.md@codepointrs.com>
2023-01-05 21:37:44 -0800

    new changes

--- src/Modules/Dashboard/Containers/Chat.js
+++ src/Modules/Dashboard/Containers/Chat.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react'
 import { Peer } from "peerjs";
+
 import Page from "../../../Components/Page";
 import CustomizedInputs from "../../../Components/TextBox";
 import Button from "../../../Components/Button";
 import AppModalPopUp from "../../../Components/AppModalPopUp";
+import { Grid, TextField, SpeedDial, SpeedDialAction } from "@mui/material";
 
+import SendIcon from '@mui/icons-material/Send';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import IosShareIcon from '@mui/icons-material/IosShare';
+import DuoIcon from '@mui/icons-material/Duo';
 
 var myId = 0;
 var render = 0;
 export default function Chat() {
     var CryptoJS = require("crypto-js");
     let mySecretKey = "HIiam@@@@RRRRRRMMMMMMMM#######";
-    let styles = { width: "fit-content", padding: "4px", borderRadius: 5, margin: 0 }
+    let styles = { width: "fit-content", padding: "4px", borderRadius: 5, margin: 0, margin: "8px 8px" };
+    let sendDiv = { width: "98%", position: "absolute", bottom: "0", left: "0", height: "40px", display: "flex", border: "1px solid #ccc", padding: "5px", margin: "5px" };
 
     const [isOpenAddmodal, setOpenAddModal] = useState(false);
     const actions = [
@@ -22,6 +29,7 @@ export default function Chat() {
             }
         }
     ];
+
     const [users, setUsers] = useState([]); //friendlist
     const [remotePeerIdValue, setRemotePeerIdValue] = useState(""); //for adding friends
     const [number, setNumber] = useState(0); //for rerendering
@@ -71,7 +79,7 @@ export default function Chat() {
         });
         // call recivieng option
         peer.on('call', (call) => {
-            console.log(call, "call recieves")
+            console.log(call, "call recieves");
             var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
             getUserMedia({ video: true, audio: true }, (mediaStream) => {
                 currentUserVideoRef.current.srcObject = mediaStream;
@@ -83,10 +91,10 @@ export default function Chat() {
                     remoteVideoRef.current.play();
                     console.log("call streaming recieves");
                 });
-                call.on("close", () => {
-                    setOpenCallSection(false);
-                });
-            });
+                // call.on("close", () => {
+                //     setOpenCallSection(false);
+                // });
+            }, err => { console.error('Error!', err) });
         });
         peerInstance.current = peer;
     }, [])
@@ -147,44 +155,50 @@ export default function Chat() {
         if (friendId) {
             setOpenCallSection(true);
             var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-            getUserMedia({ audio: true }, (mediaStream) => {
+            getUserMedia({ video: true, audio: true }, (mediaStream) => {
                 currentUserVideoRef.current.srcObject = mediaStream;
                 currentUserVideoRef.current.play();
-                const call = peerInstance.current.call(friendId, mediaStream)
+                // window.call = peerInstance.current.call(friendId, mediaStream);
+                const call = peerInstance.current.call(friendId, mediaStream);
                 console.log("call opened", call);
                 call.on('stream', (remoteStream) => {
                     remoteVideoRef.current.srcObject = remoteStream
                     remoteVideoRef.current.play();
                     console.log("call streaming ", friendId);
                 });
-                call.on("close", () => {
-                    setOpenCallSection(false);
-                });
-            }, err => { console.log('Error!') });
+                // call.on("close", () => {
+                //     setOpenCallSection(false);
+                // });
+            }, err => { console.error('Error!', err) });
         }
     }
 
     const endCall = () => {
-        setOpenCallSection(false);
+        // window.call.on("close", () => { setOpenCallSection(false); });
     }
 
     // shareScreen option
     async function shareScreen() {
-        let options = { audio: true, video: true };
+        let options = {
+            video: { displaySurface: "window" },
+            audio: { echoCancellation: true, noiseSuppression: true, sampleRate: 44100, suppressLocalAudioPlayback: true },
+            surfaceSwitching: "include", selfBrowserSurface: "exclude", systemAudio: "exclude"
+        }
         if (friendId) {
             setOpenScreenShareSection(true);
             try {
                 let captureStream = await navigator.mediaDevices.getDisplayMedia(options);
                 // *********************
-                screenShareRef.current.srcObject = captureStream;
-                screenShareRef.current.play();
+                // screenShareRef.current.srcObject = captureStream;
+                // screenShareRef.current.play();
                 // _________________
-                // const call = peerInstance.current.call(friendId, captureStream);
-                // call.on('stream', (remoteStream) => {
-                //     screenShareRef.current.srcObject = remoteStream;
-                //     screenShareRef.current.play();
-                //     console.log("sharing ", friendId);
-                // });
+                const call = peerInstance.current.call(friendId, captureStream);
+                console.log(call, "calling");
+                call.on('stream', (remoteStream) => {
+                    screenShareRef.current.srcObject = remoteStream;
+                    screenShareRef.current.play();
+                    console.log("sharing ", friendId);
+                });
                 // _________________
                 captureStream.getVideoTracks()[0].addEventListener('ended', () => {
                     console.log("ended");
@@ -204,15 +218,17 @@ export default function Chat() {
             let myUser = messageSetUsers.find(item => item.user === friendId);
             let messages = myUser.messages;
             return messages.map((item, i) => (
-                <tr key={i} style={{ height: "35px" }}>
-                    <td style={{ padding: "5px" }} colSpan={3} >
-                        <p style={item.sender === "sender" ? { background: '#ccc', ...styles } : { background: '#e2d5c5', ...styles }}>
-                            {item.message}</p>
-                    </td>
-                </tr>
+                <p key={i} style={item.sender === "sender" ? { background: '#ccc', ...styles } : { background: '#e2d5c5', ...styles }}>
+                    {item.message}
+                </p>
             ));
         }
     }, [number, render]);
+
+    const actionsSend = [
+        { icon: <DuoIcon />, name: 'Vedio Call', function: () => call },
+        { icon: <IosShareIcon />, name: 'Share Screen', function: () => shareScreen() },
+    ];
 
     return (
         <div>
@@ -239,61 +255,73 @@ export default function Chat() {
                         </table>
                     </div>
                 </AppModalPopUp>
+                {/* ADD FRIENDS IN LIST */}
 
-                {/* YOUR CHATS */}
-                {/* ++++++++++++++++++++++++++++++++++++ */}
-                <div>
-                    <table style={{ width: "80%", borderCollapse: "collapse", background: "#fff", marginBottom: "8px" }}>
-                        <tbody>
-                            <tr style={{ height: "40px", textAlign: "center" }}>
-                                <td colSpan={3}><b>Chat with : {friendId ? friendId : "----------"}</b></td>
-                            </tr>
+                <Grid container sx={{ marginTop: "1rem" }}>
+                    <Grid item md={4} xs={12} s>
+                        {/* CHAT LISTS */}
+                        <div style={{ background: "#fff", boxShadow: "1px 1px 1px 0px #ccc", width: "98%", minHeight: "80vh" }} >
+                            <table style={{ width: "100%", textAlign: "left", borderCollapse: "collapse", }}>
+                                <tbody>
+                                    <tr style={{ height: "50px", background: "rgb(244 245 246)", color: "black", }}>
+                                        <th style={{ padding: "5px" }} colSpan={2}>Users</th>
+                                    </tr>
+                                    {users.length === 0 && <tr style={{ textAlign: "left", height: "40px" }}><td style={{ padding: "5px" }} colSpan={3}>No Friends</td></tr>}
+                                    {users?.length > 0 && users.map((item, i) => (
+                                        <tr key={i} style={{ height: "40px", }}>
+                                            <td style={{ padding: "5px", width: "70%" }}  >{item}</td>
+                                            <td style={{ padding: "5px", textAlign: "right" }}><Button onClick={() => ConnectWithFriend(item)} label="Connect" /></td>
+                                        </tr>))}
+                                </tbody>
+                            </table>
+                        </div>
+                        {/* CHAT LISTS */}
+                    </Grid>
+                    {/* YOUR CHATS */}
+                    <Grid item md={8} xs={12} >
+                        <div style={{ background: "#fff", boxShadow: "1px 1px 1px 0px #ccc", minHeight: "80vh", position: "relative" }} >
+                            <table style={{ width: "100%", borderCollapse: "collapse", background: "#fff", marginBottom: "8px" }}>
+                                <tbody>
+                                    <tr style={{ height: "50px", textAlign: "left", background: "rgb(244 245 246)", color: "black" }}>
+                                        <td style={{ padding: "5px" }} colSpan={2}><b>Chat with : {friendId ? friendId : "----------"}</b></td>
+                                    </tr>
+                                    {!friendId && <tr style={{ height: "40px" }}><td style={{ textAlign: "left", padding: "5px" }}>Empty</td></tr>}
+                                </tbody>
+                            </table>
                             {renderMyChats()}
-                            {friendId && <tr>
-                                <td colSpan={2} style={{ padding: "5px" }}>
-                                    <CustomizedInputs placeholder="Enter Message" onChange={handleChange} multiline={true} value={message} />
-                                </td>
-                                <td style={{ padding: "5px", }}>
-                                    <Button onClick={send} label="Send" />
-                                    <Button style={{ marginLeft: "8px" }} onClick={call} label="Call" />
-                                    <Button style={{ marginLeft: "8px" }} onClick={shareScreen} label="Share Screen" />
-                                </td>
-                            </tr>}
-                            {!friendId && <tr><td style={{ textAlign: "center" }}>Empty</td></tr>}
-                        </tbody>
-                    </table>
-                    {/* video chat */}
-                    {isCallSection && <div style={{ height: "50vh", width: "80%", background: "#fff", marginBottom: "8px" }}>
-                        <video ref={currentUserVideoRef} />
-                        <video ref={remoteVideoRef} />
-                        <Button onClick={endCall} label="End" />
-                    </div>}
+                            {friendId && <div style={sendDiv}>
+                                <TextField sx={{ width: "90%" }} InputProps={{ disableUnderline: true }} placeholder="Enter Message" variant='standard'
+                                    multiline onChange={handleChange} value={message} />
+                                <SendIcon sx={{ fontSize: "18px", color: "#6d6868", margin: "5px" }} onClick={send} />
+                                <SpeedDial ariaLabel="SpeedDial basic example"
+                                    sx={{ position: 'relative', '& .MuiFab-primary': { backgroundColor: '#ccc', color: 'white', width: 40, height: 50, margin: -0.3, } }} direction={"up"}
+                                    icon={<SpeedDialIcon />}
+                                >
+                                    {actionsSend.map((action) => (
+                                        <SpeedDialAction
+                                            key={action.name}
+                                            icon={action.icon}
+                                            tooltipTitle={action.name}
+                                            onClick={action.function}
+                                        />
+                                    ))}
+                                </SpeedDial>
+                            </div>}
+                            {/* video chat */}
+                            {isCallSection && <div style={{ height: "50vh", width: "80%", background: "#fff", marginBottom: "8px" }}>
+                                <video ref={currentUserVideoRef} />
+                                <video ref={remoteVideoRef} />
+                                <Button onClick={endCall} label="End" />
+                            </div>}
+                            {/* screen sharing */}
+                            {isScreenShareSection && <div style={{ height: "100vh", width: "80%", background: "#fff", marginBottom: "8px" }}>
+                                <video style={{ height: "100%", width: "100%" }} controls={true} muted={true} ref={screenShareRef} />
+                            </div>}
+                        </div>
+                    </Grid>
+                    {/* YOUR CHATS */}
+                </Grid>
 
-                    {/* screen sharing */}
-                    {isScreenShareSection && <div style={{ height: "100vh", width: "80%", background: "#fff", marginBottom: "8px" }}>
-                        <video style={{ height: "100%", width: "100%" }} controls={false} muted={true} ref={screenShareRef} />
-                    </div>}
-                </div>
-                {/* ++++++++++++++++++++++++++++++++++++ */}
-
-
-                {/* CHAT LISTS */}
-                <div style={{ marginBottom: "8px" }}>
-                    <table style={{ width: "80%", textAlign: "left", borderCollapse: "collapse", background: "#fff", }}>
-                        <tbody>
-                            <tr style={{ height: "50px", background: "#ccc", color: "#fff", }}>
-                                <th style={{ padding: "5px", width: "70%", }} colSpan={2}>Users</th>
-                                <th style={{ padding: "5px" }}>Action</th>
-                            </tr>
-                            {users.length === 0 && <tr style={{ textAlign: "center", height: "40px" }}><td colSpan={3}>No Friends</td></tr>}
-                            {users?.length > 0 && users.map((item, i) => (
-                                <tr key={i} style={{ height: "40px", }}>
-                                    <td style={{ padding: "5px", width: "70%", }} colSpan={2} >{item}</td>
-                                    <td style={{ padding: "5px" }}><Button onClick={() => ConnectWithFriend(item)} label="Connect" /></td>
-                                </tr>))}
-                        </tbody>
-                    </table>
-                </div>
 
             </Page>
         </div>
